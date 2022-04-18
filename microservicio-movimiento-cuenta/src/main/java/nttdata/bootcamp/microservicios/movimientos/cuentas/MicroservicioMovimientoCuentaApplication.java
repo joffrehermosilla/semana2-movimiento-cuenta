b2b1faaf21b57f65da7a4ed1cd5c34e32ae82af2
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 23:49:20 -0500

    primer commit

---
+++ microservicio-movimiento-cuenta/src/main/java/nttdata/bootcamp/microservicios/movimientos/cuentas/MicroservicioMovimientoCuentaApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.movimientos.cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioMovimientoCuentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioMovimientoCuentaApplication.class, args);
+	}
+
+}
---
+++ microservicio-movimiento-cuenta/src/test/java/nttdata/bootcamp/microservicios/movimientos/cuentas/MicroservicioMovimientoCuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.movimientos.cuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioMovimientoCuentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
