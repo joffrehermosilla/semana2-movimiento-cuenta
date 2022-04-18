9e044c39d4f028aef25f3e355b712f1021dc13e7
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-04-18 14:47:45 -0500

    configuracion config server

--- microservicio-movimiento-cuenta/src/main/java/nttdata/bootcamp/microservicios/movimientos/cuentas/MicroservicioMovimientoCuentaApplication.java
+++ microservicio-movimiento-cuenta/src/main/java/nttdata/bootcamp/microservicios/movimientos/cuentas/MicroservicioMovimientoCuentaApplication.java
@@ -2,7 +2,10 @@ package nttdata.bootcamp.microservicios.movimientos.cuentas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicioMovimientoCuentaApplication {
 
